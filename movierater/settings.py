c715bd17f7b99bcd2ba1817ae54f7e5fac09fdd1
jen615 <63896201+jen615@users.noreply.github.com>
2021-01-21 09:02:04 -0800

    secret

--- movierater/settings.py
+++ movierater/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k$9$yys_zb+iym3e2u6%lp2s2a#5$q)6&^&%fzl=6m_es@@b8+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
